cd57df87920ae8cdffe593c618a32a969601392b
Anbesha Thapa <anbesha@exmple.com>
2022-08-31 21:47:04 +0545

    edit

---
+++ android/app/src/main/kotlin/com/example/bhariya_rider_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bhariya_rider_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bhariya_rider_app/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashScreen(),
+      },
+    );
+  }
+}
